11d9872d95036c7f509415607d7e1ea6c4f08df3
Rodrigo <rodrigofbarrios2803@gmail.com>
2023-03-23 05:59:06 -0300

    APIs upcoming y past

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,72 +1,277 @@
 
-
-fetch("https://mindhub-ab35.onrender.com/api/amazing-events")
-    .then(response => response.json())
-    .then(data => {
-
-        let maxAttendance = 0;
-        let eventWithMaxAttendance = null;
-        let minAttendance = 100;
-        let eventWithMinAttendance = null;
-        let eventWithLargerCapacity = null;
-        let eventFuture = data.events.filter(each => each.date > data.currentDate) 
-        let eventPast = data.events.filter(each => each.date < data.currentDate) 
-        let highAssistance = assistance(eventPast)[0]
-        let lowAssistance = assistance(eventPast).reverse()[0]
-        let mostCapacity = capacity(data.events)
-        
-        
-        printFirstColumn(highAssistance,lowAssistance,mostCapacity )
-        
-     
-    })
-
-
-function capacity(array) {
-let array_capacity = array.map(events => {
-    return {
-        capacity: events.capacity, 
-        name: events.name,
-    }
-})
-array_capacity.sort((a, b) => b.capacity - a.capacity)
-return array_capacity[0]
-}
-
-function assistance(array) {
-    let array_assistance = array.map(events => {
-        return {
-            assistance: events.assistance, 
-            name: events.name,
-        }
-    })
-    array_assistance.sort((a, b) => b.assistance - a.assistance)
-   /*  console.log(array_assistance) */
-    return array_assistance
-    }
-
-function printFirstColumn(highAssistance, lowAssistance, capacity){
-    const firstTable = document.getElementById("eventsStatsBody")
-    firstTable.innerHTML = `<tr>
-    <td>${highAssistance.name}</td>
-    <td>${lowAssistance.name}</td>
-    <td>${capacity.name}</td>
-  </tr>
-    
-    `
-}
-
-function category(array) {
-    let array_category = array.map(events => {
-        return {
-            category: events.category, 
-            name: events.name,
-        }
-    })
-    array_category.sort((a, b) => b.category - a.category)
-    console.log(array_category)
-    return array_category[0]
-    }
-
-
-
+ let fetchURL = "https://mindhub-ab35.onrender.com/api/amazing-events";
+ let pastFetchURL = fetchURL + "?time=past";
+ let upcomingFetchURL = fetchURL + "?time=upcoming";
+ let categoriesFetchURL = fetchURL + "?category=";
+ 
+ let highestEvents = [];
+ let categoriesPast = [];
+ 
+ let innerTablePast = ``;
+ 
+ async function eventsStatsFn() {
+   try {
+     let capturedElement = document.getElementById("eventsStatsBody");
+ 
+     let dataEvents = await fetch(pastFetchURL).then((response) =>
+       response.json()
+     );
+ 
+     let attendanceEvents = dataEvents.events.map((each) => {
+       return {
+         name: each.name,
+         assistance: each.assistance,
+         capacity: each.capacity,
+         percentageAttendance: (each.assistance * 100) / each.capacity,
+       };
+     });
+ 
+     attendanceEvents.sort(function (a, b) {
+       return b.percentageAttendance - a.percentageAttendance;
+     });
+ 
+     highestEvents = attendanceEvents.map((each) => {
+       return each;
+     });
+ 
+     attendanceEvents.sort(function (a, b) {
+       return b.capacity - a.capacity;
+     });
+ 
+     innerTablePast = `
+ <tr>
+   <td><p>${
+     highestEvents[0].name
+   }</p><p class="events-number">${highestEvents[0].percentageAttendance.toFixed(
+       2
+     )} %</p></td>
+   <td><p>${
+     highestEvents[highestEvents.length - 1].name
+   }</p><p class="events-number">${highestEvents[
+       highestEvents.length - 1
+     ].percentageAttendance.toFixed(2)} %</p></td>
+   <td><p>${
+     attendanceEvents[0].name
+   }</p><p>${attendanceEvents[0].capacity.toLocaleString(
+       "en-US"
+     )} spectators</p></td>
+ </tr>`;
+ 
+     capturedElement.innerHTML = innerTablePast;
+   } catch (error) {
+     console.log(error);
+   }
+ }
+ 
+ eventsStatsFn();
+ 
+ async function upcomingEventsStats() {
+   try {
+     let innerTableUpcoming = ``;
+     let categoriesUpcoming = [];
+ 
+     let capturedElement = document.getElementById("upcomingStatsBody");
+ 
+     let dataEvents = await fetch(upcomingFetchURL).then((response) =>
+       response.json()
+     );
+ 
+     let sortByCategories = dataEvents.events.map((each) => {
+       return {
+         name: each.name,
+         category: each.category,
+         estimate: each.estimate,
+         capacity: each.capacity,
+         price: each.price,
+       };
+     });
+ 
+     sortByCategories.sort(function (a, b) {
+       let x = a.category.toLowerCase();
+       let y = b.category.toLowerCase();
+       if (x < y) {
+         return -1;
+       }
+       if (x > y) {
+         return 1;
+       }
+       return 0;
+     });
+ 
+     console.log(sortByCategories);
+ 
+     let eventsCategories = sortByCategories.map((each) => {
+       return each.category;
+     });
+ 
+     eventsCategories = new Set(eventsCategories);
+     eventsCategories = Array.from(eventsCategories);
+ 
+     console.log(eventsCategories);
+ 
+     for (let objectEvent of sortByCategories) {
+       if (categoriesUpcoming.length == 0) {
+         categoriesUpcoming.push({
+           name: objectEvent.category,
+           revenue: objectEvent.estimate * objectEvent.price,
+           estimate: objectEvent.estimate,
+           capacity: objectEvent.capacity,
+         });
+       } else {
+         if (
+           categoriesUpcoming[categoriesUpcoming.length - 1].name ==
+           objectEvent.category
+         ) {
+           categoriesUpcoming[categoriesUpcoming.length - 1] = {
+             name: objectEvent.category,
+             revenue:
+               objectEvent.estimate * objectEvent.price +
+               categoriesUpcoming[categoriesUpcoming.length - 1].revenue,
+             estimate:
+               objectEvent.estimate +
+               categoriesUpcoming[categoriesUpcoming.length - 1].estimate,
+             capacity:
+               objectEvent.capacity +
+               categoriesUpcoming[categoriesUpcoming.length - 1].capacity,
+           };
+         } else {
+           categoriesUpcoming.push({
+             name: objectEvent.category,
+             revenue: objectEvent.estimate * objectEvent.price,
+             estimate: objectEvent.estimate,
+             capacity: objectEvent.capacity,
+           });
+         }
+       }
+     }
+ 
+     for (let i = 0; i < categoriesUpcoming.length; i++) {
+       innerTableUpcoming =
+         innerTableUpcoming +
+         `
+         <tr>
+         <td>
+         <p>${categoriesUpcoming[i].name}</p>
+         </td>
+         <td>
+         <p>&#36; ${categoriesUpcoming[i].revenue.toLocaleString("en-US")}</p>
+         </td>
+         <td>
+         <p>${(
+           (categoriesUpcoming[i].estimate * 100) /
+           categoriesUpcoming[i].capacity
+         ).toFixed(2)} %</p>
+         </td>
+         </tr>`;
+     }
+ 
+     capturedElement.innerHTML = innerTableUpcoming;
+   } catch (error) {
+     console.log(error);
+   }
+ }
+ 
+ upcomingEventsStats();
+ 
+ async function pastEventsStats() {
+   try {
+     let sortByCategories = [];
+ 
+     let innerTableUpcoming = ``;
+ 
+     let capturedElement = document.getElementById("pastStatsBody");
+ 
+     let dataEvents = await fetch(pastFetchURL).then((response) =>
+       response.json()
+     );
+ 
+     for (let objectEvent of dataEvents.events) {
+       sortByCategories.push({
+         name: objectEvent.name,
+         category: objectEvent.category,
+         assistance: objectEvent.assistance,
+         capacity: objectEvent.capacity,
+         price: objectEvent.price,
+       });
+     }
+ 
+     sortByCategories.sort(function (a, b) {
+       let x = a.category.toLowerCase();
+       let y = b.category.toLowerCase();
+       if (x < y) {
+         return -1;
+       }
+       if (x > y) {
+         return 1;
+       }
+       return 0;
+     });
+ 
+     console.log(sortByCategories);
+ 
+     categoriesUpcoming = [];
+ 
+     for (let objectEvent of sortByCategories) {
+       if (categoriesUpcoming.length == 0) {
+         categoriesUpcoming.push({
+           name: objectEvent.category,
+           revenue: objectEvent.assistance * objectEvent.price,
+           assistance: objectEvent.assistance,
+           capacity: objectEvent.capacity,
+         });
+       } else {
+         if (
+           categoriesUpcoming[categoriesUpcoming.length - 1].name ==
+           objectEvent.category
+         ) {
+           categoriesUpcoming[categoriesUpcoming.length - 1] = {
+             name: objectEvent.category,
+             revenue:
+               objectEvent.assistance * objectEvent.price +
+               categoriesUpcoming[categoriesUpcoming.length - 1].revenue,
+             assistance:
+               objectEvent.assistance +
+               categoriesUpcoming[categoriesUpcoming.length - 1].assistance,
+             capacity:
+               objectEvent.capacity +
+               categoriesUpcoming[categoriesUpcoming.length - 1].capacity,
+           };
+         } else {
+           categoriesUpcoming.push({
+             name: objectEvent.category,
+             revenue: objectEvent.assistance * objectEvent.price,
+             assistance: objectEvent.assistance,
+             capacity: objectEvent.capacity,
+           });
+         }
+       }
+     }
+     console.log(categoriesUpcoming);
+ 
+     for (let i = 0; i < categoriesUpcoming.length; i++) {
+       innerTableUpcoming =
+         innerTableUpcoming +
+         `
+         <tr>
+         <td>
+         <p>${categoriesUpcoming[i].name}</p>
+         </td>
+         <td>
+         <p>&#36; ${categoriesUpcoming[i].revenue.toLocaleString("en-US")}</p>
+         </td>
+         <td>
+         <p>${(
+           (categoriesUpcoming[i].assistance * 100) /
+           categoriesUpcoming[i].capacity
+         ).toFixed(2)} %</p>
+         </td>
+         </tr>`;
+     }
+ 
+     capturedElement.innerHTML = innerTableUpcoming;
+   } catch (error) {
+     console.log(error);
+   }
+ }
+ 
+ pastEventsStats();
--- scripts/upcoming_events.js
+++ scripts/upcoming_events.js
@@ -1,10 +1,22 @@
-/* console.log(data) */
-const eventos = data.events
-let cardsDeEventos = []
-let numero = 3
-let ancho = `20rem`
-for (let evento of eventos) {
-  if (data.currentDate < evento.date) {
+let urlApi = "https://mindhub-ab35.onrender.com/api/amazing-events"
+
+async function traerDatos(){
+  const response = await fetch(urlApi)
+  /* const responseJson = await fetch("./amazing-blue.json") */
+  console.log(response)
+  const datos = await response.json()
+  console.log(datos);
+  printCards(datos.events)
+}
+
+traerDatos()
+
+function printCards(events) {
+  let cardsDeEventos = []
+  let numero = 3
+  let ancho = `20rem`
+  for (let evento of events) {
+    if (data.currentDate < evento.date) {
     let card = `<div class="card" style="width: ${ancho};">
     <img src="${evento.image}" class="card-img-top" alt="${evento.name}">
     <div class="card-body">
@@ -14,12 +26,10 @@ for (let evento of eventos) {
     </div>
   </div>`
 
-   /*  console.log(card) */
+
     cardsDeEventos.push(card)
   }
-}
-function printCards() {
   let conteiner1 = document.querySelector(`#card_conteiner`)
   conteiner1.innerHTML = cardsDeEventos.join(``)
 }
-printCards()
+}
